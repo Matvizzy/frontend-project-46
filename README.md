8964dba7ce8c7eee4a8530fbbd37bc433ee5726d
Matvizzy <matvey.surow06@gmail.com>
2024-06-17 03:52:33 +0300

    fix

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Matvizzy/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Matvizzy/frontend-project-46/actions)
+
+### Codeclimate
 [![Maintainability](https://api.codeclimate.com/v1/badges/0b27228e563fe974bf24/maintainability)](https://codeclimate.com/github/Matvizzy/frontend-project-46/maintainability)
+
+### Git actions
 [![build](https://github.com/Matvizzy/frontend-project-46/actions/workflows/auto-tests.yml/badge.svg)](https://github.com/Matvizzy/frontend-project-46/actions/workflows/auto-tests.yml)
--- src/index.js
+++ src/index.js
@@ -13,17 +13,25 @@ const gendiff = (filepath1, filepath2) => {
 
   const keys = (_.union(_.keys(data1), _.keys(data2))).sort();
   const diffObj = keys.map((key) => {
+    // есть в первом, но нет во втором
     if (_.has(data1, key) && !_.has(data2, key)) {
       return `  - ${key}: ${data1[key]}`;
     }
+
+    // есть во втором, но нет в первом
     if (_.has(data2, key) && !_.has(data1, key)) {
       return `  + ${key}: ${data2[key]}`;
     }
+
+    // есть в обоих, но значения разные
     if (data1[key] !== data2[key]) {
       return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
     }
+
+    // есть в обоих, но значения одинаковые
     return `    ${key}: ${data1[key]}`;
   })
+  
   return `{\n${diffObj.join('\n')}\n}`;
 }
 
